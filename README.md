78a7555c4be65383b79213221239cd2abb0253f7
刘建环 <liujianhuan@360.cn>
2017-02-08 19:50:38 +0800

    pages

--- README.md
+++
@@ -1,2 +0,0 @@
-# vueAdmin
-采用Vue+Vue-router+element-ui实现的后台管理模板
--- src/components/layout/index.js
+++
@@ -1,7 +0,0 @@
-export Navbar from './Navbar'
-
-export Sidebar from './Sidebar'
-
-export AppMain from './AppMain'
-
-export FooterBar from './FooterBar'
