a83101aa07de9f27c3dd000ab23c338cf512889c
刘建环 <liujianhuan@360.cn>
2017-02-08 11:13:19 +0800

    改用element作为组件库

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1>{{ msg }}</h1>
+    <el-button @click.native="startHacking">Let's do it</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  data () {
+    return {
+      msg: 'Use Vue 2.0 Today!'
+    }
+  },
+
+  methods: {
+    startHacking () {
+      this.$notify({
+        title: 'It Works',
+        message: 'We have laid the groundwork for you. Now it\'s your time to build something epic!',
+        duration: 6000
+      })
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Helvetica, sans-serif;
 }
 </style>
